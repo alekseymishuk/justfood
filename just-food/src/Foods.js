6b7608b672884396159539821c8e70c8e059674e
aleksetymishuk <alekseymishuk@gmail.com>
2024-08-11 13:40:13 +0200

    refactor

--- just-food/src/Foods.js
+++ just-food/src/Foods.js
@@ -1,11 +1,11 @@
 import React, { Fragment, useState } from "react";
 import foodStyles from "./Foods.module.css";
 import FoodOrder from "./FoodOrder";
-const Foods = (props) => {
+const Foods = ({ foodItems }) => {
   const [selectedFood, setSelectedFood] = useState("");
   const handleSelect = (event) => {
     setSelectedFood(
-      props.foodItems.find((item) => {
+      foodItems.find((item) => {
         return item.id === parseInt(event.currentTarget.dataset.id);
       })
     );
@@ -17,7 +17,7 @@ const Foods = (props) => {
         <Fragment>
           <h4 className={foodStyles.foodTitle}>Choose from our Menu</h4>
           <ul className={foodStyles.ulFoods}>
-            {props.foodItems.map((item) => {
+            {foodItems.map((item) => {
               return (
                 <li
                   key={item.id}
